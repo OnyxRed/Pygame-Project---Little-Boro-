8b87f1289a3250eb93c4ee82ce0e14054d1d9553
willmac1 <maclachlanwill@gmail.com>
2015-05-17 17:38:50 +0100

    def __init__

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 __version__="0.1"
 
 #We should probably begin with a generic class datatype, which I usually call 'object'
+
 class Object(object):
+    def __init__(self,pos,width,height):
+        self.pos=pos
+        self.width=width
+        self.height=height
+
