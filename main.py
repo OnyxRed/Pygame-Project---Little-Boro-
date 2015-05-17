01e30e68a5076129b01b193cad4795541b9475c4
willmac1 <maclachlanwill@gmail.com>
2015-05-17 19:16:04 +0100

    Fixing children
    Children are now represented in a dictionary, rather than a list...

--- main.py
+++ main.py
@@ -8,14 +8,29 @@ pygame.init()
 #We should probably begin with a generic class datatype, which I usually call 'object'
 
 class Object(object): #Generic class objects
-    def __init__(self,pos,width,height,*arg,**kwargs):
+    def __init__(self,pos,width,height,image=None,color=None,state=None,*arg,**kwargs):
         self.pos=pos
         self.width=width
         self.height=height
+        self.image=image
+        self.color=color
+        self.state=state
+
+    def getRect(self):
+        return pygame.Rect(self.pos,(self.width,self.height))
+
+    def draw(self,screen):
+        if self.image is not None:
+            screen.blit(self.image[self.state],self.getRect())
+
+        else:
+            pygame.draw.rect(screen,self.color,self.getRect())
 
 class Player(Object): #Player object
     def __init__(self,*arg,**kwargs):
         super(Player,self).__init__(*arg,**kwargs)
+        
+        
 
 class Env(object): #Created basic environment
     def __init__(self, bg,children,width,height,camera=None):
@@ -36,17 +51,21 @@ class Env(object): #Created basic environment
     def draw(self):
         self.screen.blit(self.bg,pygame.Rect(0,0,self.width,self.height))
 
+        self.children["Player"].draw()
+
     def events(self):
         for event in pygame.event.get():
             if event.type==QUIT:
                 pygame.quit()
                 sys.exit()
 
+            if even
+
     def update(self):
         pygame.display.update()
         self.clock.tick(50)
         
-root=Env(bg=pygame.image.load("images/sky/sky.png"),children=[Player(pos=(100,100),width=50,height=50)],width=1000,height=600)
+root=Env(bg=pygame.image.load("images/sky/sky.png"),children={"Player":Player(pos=(100,100),width=50,height=50,image=["images/boro/static.png"],state=0}],width=1000,height=600)
 root.run()
     
     
