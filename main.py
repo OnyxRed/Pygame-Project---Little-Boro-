544fe346515e5175191b72f454e91536815ff2ed
willmac1 <maclachlanwill@gmail.com>
2015-05-17 19:01:28 +0100

    New version (not for sam)

--- main.py
+++ main.py
@@ -1,10 +1,71 @@
-__version__="0.1"
+__version__="0.01a"
+
+import pygame,sys
+from pygame.locals import*
+
+pygame.init()
 
 #We should probably begin with a generic class datatype, which I usually call 'object'
 
-class Object(object):
-    def __init__(self,pos,width,height):
+class Object(object): #Generic class objects
+    def __init__(self,pos,width,height,image=None,color=None,*arg,**kwargs):
         self.pos=pos
         self.width=width
         self.height=height
+        self.image=image
+        self.color=color
+
+    def getRect(self):
+        return pygame.Rect(self.pos,(self.width,self.height))
+
+class Player(Object): #Player object
+    def __init__(self,*arg,**kwargs):
+        super(Player,self).__init__(*arg,**kwargs)
+
+    def draw(self,screen):
+        if self.image is not None:
+            screen.blit(self.image,self.getRect())
+
+        else:
+            pygame.draw.rect(screen,self.color,self.getRect())
+        
+        
+
+class Env(object): #Created basic environment
+    def __init__(self, bg,children,width,height,camera=None):
+        self.screen=pygame.display.set_mode((width,height))
+        self.bg=pygame.transform.scale(bg,(width,height))
+        self.children=children
+        self.camera=camera
+        self.width=width
+        self.height=height
+        self.clock=pygame.time.Clock()
+
+    def run(self):
+        while True:
+            self.draw()
+            self.events()
+            self.update()
+
+    def draw(self):
+        self.screen.blit(self.bg,pygame.Rect(0,0,self.width,self.height))
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+                sys.exit()
+
+    def update(self):
+        pygame.display.update()
+        self.clock.tick(50)
+        
+root=Env(bg=pygame.image.load("images/sky/sky.png"),children=[Player(pos=(100,100),width=50,height=50)],width=1000,height=600)
+root.run()
+    
+    
+        
+
+    
+
 
